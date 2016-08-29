30962b314ddc67064e975191157c9967b96d85d5
Lochie Ferrier <lochie@mit.edu>
2016-08-29 22:08:16 +0200

    Create Model and PosynomialInequality class, implement necessary operator overloads for the simple example
    Got started adding correct overloads and two new classes. Disappointingly cannot find a workaround for the Number class prototype sets, requiring a call in the file that is using gpkit.js. The correct setting code is wrapped up in setupNums. Next task is to add JSON file read in on the flask side, and conversion.

--- examples/simple/app/static/gpkit_unbundled.js
+++ examples/simple/app/static/gpkit_unbundled.js
@@ -1,14 +1,187 @@
+//Global ID counter, used to assign a unique ID to each variable
+ID = 0
 
+//ID assignment function
+assignID = function(){
+    id = ID
+    ID++;
+    return id
+}
+
+//Variable object, derived from gpkit. ID property is hidden to the user, and is used for JSON indexing
 Variable = function (name,val) {
-    // console.log(arguments[0]);
-
     this.name = name
     this.val = val
-    // console.log(name,val)
+    this.ID = assignID()
     this.__plus = function (leftOperand) {
-        console.log("adding Count");
-        return 909;
+        console.log("less than or equal");
+        return rightOperand
     };
-
+    this.__lessThanEqual = function (leftOperand) {
+        console.log("less than or equal");
+        var inequality = new PosynomialInequality(leftOperand,'<=',this)
+        return inequality
+    };
+    this.__greaterThanEqual = function (leftOperand) {
+        console.log("greater than or equal");
+        var inequality = new PosynomialInequality(leftOperand,'>=',this)
+        return inequality
+    };
+    // this.serialize = function () {
+    //     return JSON.stringify(this);
+    // };
 };
+
+PosynomialInequality  = function(left,oper,right){
+    this.left = left
+    this.oper = oper
+    this.right = right
+    // this.serialize = function(){
+    //     return JSON.stringify(this);
+    // }
+}
+
+Model = function (cost,constraints){
+    console.log('created model')
+    this.cost = cost
+    this.constraints = constraints
+    this.solve = function(solve){
+        console.log(this.serialize());
+    }
+    this.serialize = function(){
+        return JSON.stringify(this);
+    }
+}
+
+setupNums = function(){
+    Number.prototype.__lessThanEqual = function (leftOperand) {
+        var inequality = new PosynomialInequality(leftOperand,'<=',this)
+        return inequality
+    };
+    Number.prototype.serialize = function () {
+        return JSON.stringify(this)
+    };
+}
+
 overload = require('operator-overloading')
+
+
+// // SANDBOX
+// var overload = require('operator-overloading');
+
+
+// //An example Constructor Class
+// function Count(val) {
+//     var _this = this;
+//     this.val = val;
+//     this.__plus = function (leftOperand) {
+//         console.log("adding Count");
+//         leftOperand.val += _this.val;
+//         return this;
+//     };
+
+//     this.__doubleEqual = function (leftOperand) {
+//         console.log('double Equal Check');
+//         return _this.val == leftOperand.val;
+//     };
+
+//     this.__tripleEqual = function (leftOperand) {
+//         console.log('triple Equal Check');
+//         return _this.val === leftOperand.val;
+//     };
+//     this.__lessThanEqual = function(leftOperand){
+//         console.log('less than equal')
+//         console.log(leftOperand)
+
+//     }
+// }
+
+// //We can put in Native types too
+// String.prototype.__plus = function (leftOperand) {
+//     return (leftOperand + " <added> " + this);
+// };
+
+// //Number example
+// Number.prototype.__plus = function (leftOperand) {
+//     console.log('Adding:', leftOperand, '+', this.valueOf());
+//     return leftOperand + this;
+// };
+
+// //Number example
+// Number.prototype.__lessThanEqual = function (leftOperand) {
+//     // console.log('Adding:', leftOperand, '+', this.valueOf());
+//     console.log('number fired')
+// };
+
+// var v1 = new Count(10);
+// var v2 = new Count(20);
+// var v3 = new Count(30);
+
+// //That's how you do it. Ity has its own context scope
+// var run = overload(function (v1, v2, v3) {
+
+//     var res = v1 + v2 + v3;
+
+//     console.log(3 + 44 + 100);
+
+//     console.log('v1', v1);
+//     console.log('v2', v2);
+//     console.log('v3', v3);
+//     console.log('res', res);
+
+//     // console.log(v1 == v2);
+//     // console.log(v1 === v2);
+
+//     console.log('hello' + 'yello' + 'fellow' + 'yo!');
+
+//     console.log(33 + (3 + 3) + 55);
+
+//     var t = 33 || 44;
+//     t = 33 && 44;
+//     t = 33 & 44;
+//     t = 33 | 44;
+//     t = 33 ^ 44;
+//     t = 33 != 44;
+//     t = 33 !== 44;
+//     t = 33 < 44;
+//     t = 33 > 44;
+//     t = 33 >= 44;
+//     t = 33 <= 44;
+//     t = 33 in [44];
+//     t = 33 instanceof Number;
+//     t = 33 << 44;
+//     t = 33 >> 44;
+//     t = 33 >>> 44;
+//     t = 33 - 44;
+//     t = 33 * 44;
+//     t = 33 / 44;
+//     t = 33 % 44;
+//     t = -44;
+//     t = +44;
+//     t = ~44;
+//     t = ++v1;
+//     t = --v1;
+//     t = !v1;
+//     t += v1;
+//     t /= !v2;
+//     t *= !v2;
+//     t -= !v2;
+//     t %= !v2;
+//     t <<= !v2;
+//     t >>= !v2;
+//     t >>>= !v2;
+//     t &= !v2;
+//     t ^= !v2;
+//     t |= !v2;
+//     console.log('firing leq')
+//     t = v1 <= 10
+//     console.log(t)
+//     t = v1 + v2 * (!v1 || !v2 && 22) + 33 * 55 / ((4 | ~555) * ~~v2 * +new Date());
+//     console.log(t);
+
+// }); //Do this to enable operator overloading in this function. We don't recommend global enablement as that would be confusing.
+
+// //This will be normal operation as defined in JS.
+// console.log(3 + 44 + 100);
+
+// run(v1, v2, v3);
