edde7211ef7a90c9cb0237d2486656f02a60b3e0
Lochie Ferrier <lochie@mit.edu>
2016-08-30 15:18:04 +0200

    Clean up views.py, add box example

--- examples/app/gpkitjs.py
+++ examples/app/gpkitjs.py
@@ -1,4 +1,11 @@
 # http://stackoverflow.com/questions/3768895/how-to-make-a-class-json-serializable
+import gpkit
+from json import JSONEncoder
+import json
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__ 
 
 class Solution(object):
 	""" A model of a component's power:
@@ -17,3 +24,50 @@ class Variable(object):
 		self.name = name
 		self.value = value
 		self.ID = ID
+
+class Solver(object):
+	def __init__(self):
+		self.modelDict = {}
+	def initFromRequest(self,request):
+		  r= request.form
+		  for line in r:
+		  	resultString = str(line);
+		  self.modelDict = json.loads(resultString)
+	def solve(self):
+	  constraints = []
+	  varDict = {}
+	  for constraint in self.modelDict["constraints"]:
+	  	if type(constraint["left"]) == dict:
+	  		varProperties = constraint["left"]
+	  		# inputVarsDict[varProperties["ID"]] = varProperties
+	  		tempVar = gpkit.Variable(varProperties["name"])
+	  		constraint["left"] = tempVar
+	  		print(varProperties["ID"])
+	  		varDict[varProperties["ID"]] = tempVar
+	  	if constraint["oper"] == "geq":
+	  		constraints +=[constraint["left"] >= constraint["right"]]
+	  costDict = self.modelDict["cost"]
+	  cost = varDict[costDict["ID"]]
+	  # costVar = Variable(cost["name"])
+	  # constraint["left"] = tempVar
+	  print('final inputs to normal model')
+	  x = gpkit.Variable('x')
+	  constraintsNormal = []
+	  constraintsNormal += [x>=1]
+	  objectiveNormal = x
+	  print(objectiveNormal)
+	  print(constraintsNormal)
+
+	  mNormal = gpkit.Model(objectiveNormal,constraintsNormal)
+	 
+	  print('final inputs to JS model')
+	  print(cost)
+	  print(constraints)
+	  m = gpkit.Model(cost,constraints)
+	  sol = m.solve(verbosity=1)
+	  print('solution dict')
+	  print(sol.program.result["variables"])
+	  jsSol = Solution()
+	  jsSol.translateSol(sol,varDict)
+	  output = MyEncoder().encode(jsSol)
+	  return output
--- examples/app/views.py
+++ examples/app/views.py
@@ -8,11 +8,7 @@ from wtforms.validators import DataRequired
 from flask import request
 from json import JSONEncoder
 import json
-import gpkitjs
-
-class MyEncoder(JSONEncoder):
-    def default(self, o):
-        return o.__dict__  
+import gpkitjs 
 
 @app.route('/')
 @app.route('/index')
@@ -32,52 +28,6 @@ def indexUpdate():
   print(r)
   for line in r:
   	resultString = str(line);
-  ModelDict = json.loads(resultString)
-  constraints = []
-  varDict = {}
-  for constraint in ModelDict["constraints"]:
-  	if type(constraint["left"]) == dict:
-  		varProperties = constraint["left"]
-  		# inputVarsDict[varProperties["ID"]] = varProperties
-  		tempVar = Variable(varProperties["name"])
-  		constraint["left"] = tempVar
-  		print(varProperties["ID"])
-  		varDict[varProperties["ID"]] = tempVar
-  	if constraint["oper"] == "geq":
-  		constraints +=[constraint["left"] >= constraint["right"]]
-  costDict = ModelDict["cost"]
-  cost = varDict[costDict["ID"]]
-  # costVar = Variable(cost["name"])
-  # constraint["left"] = tempVar
-  print('final inputs to normal model')
-  x = Variable('x')
-  constraintsNormal = []
-  constraintsNormal += [x>=1]
-  objectiveNormal = x
-  print(objectiveNormal)
-  print(constraintsNormal)
-
-  mNormal = Model(objectiveNormal,constraintsNormal)
- 
-  print('final inputs to JS model')
-  print(cost)
-  print(constraints)
-  m = Model(cost,constraints)
-  sol = m.solve(verbosity=1)
-  print('solution dict')
-  print(sol.program.result["variables"])
-  jsSol = gpkitjs.Solution()
-  jsSol.translateSol(sol,varDict)
-  output = MyEncoder().encode(jsSol)
-  # print output
-  # print(varDict)
-  # print r 
-  # # print r[0]
-  # a = []
-  # for line in r:
-  #   print 'new line'
-
-  #   a = (json.loads(line))
-  # print(str(a))
-  # return str(a)
-  return output
+  solver = gpkitjs.Solver();
+  solver.initFromRequest(request)
+  return solver.solve()
